9a09dfd6bfd9b391fee5017b717e354371a4d411
Eric Casanova <kilbane1407@gmail.com>
2023-07-22 17:50:10 -0300

    added PR webhook controller

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -3,5 +3,5 @@ import WebhookController from "../controllers/webhook.controller.js";
 const router = Router();
 const controller = new WebhookController();
 router.post("/push", controller.postPushWebhook);
-router.post("/pr");
+router.post("/pr", controller.postPRWebhook);
 export default router;
