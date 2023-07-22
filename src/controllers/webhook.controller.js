d0251b87f1b64bf2ebcc9da41edd1281584bd8f1
Eric Casanova <kilbane1407@gmail.com>
2023-07-22 17:02:18 -0300

    added devBot publish webhook event

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -1,7 +1,14 @@
+import DevBot from "../bot.js";
 export default class WebhookController {
   async postWebhook(req, res) {
     try {
       console.log(req.body);
+      const msg = `${req.body.pusher.name} hizo un push al repo ${body.repository.name}
+      ${req.body.head_commit.message}
+      time: ${req.body.head_commit.timestamp}
+      url: ${req.body.head_commit.url}
+      `;
+      DevBot.instance.publishMessage(msg);
       res.sendStatus(200);
     } catch (error) {
       res.status(400).json({ error });
